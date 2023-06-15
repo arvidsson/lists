e0b457e5c686f788f7a7f553471de990d0774b6c
arvidsson <arvidsson@gmail.com>
2023-06-15 08:14:49 +0200

    render items

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,16 +1,47 @@
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
-import { addDoc, collection } from 'firebase/firestore';
+import { View, Text, Button, TextInput, StyleSheet, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { addDoc, collection, onSnapshot } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
 
+export interface Item {
+  id: string;
+  title: string;
+  isDone: boolean;
+}
+
 const List = () => {
-  const [items, setItems] = useState<any[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
   const [item, setItem] = useState('');
+
+  useEffect(() => {
+    const itemsRef = collection(db, 'items');
+
+    const subscriber = onSnapshot(itemsRef, {
+      next: (snapshot) => {
+        const items: Item[] = [];
+        snapshot.docs.forEach(doc => {
+          items.push({
+            id: doc.id,
+            ...doc.data()
+          } as Item);
+        });
+        setItems(items);
+      }
+    });
+
+    return () => subscriber();
+  }, []);
 
   const addItem = async () => {
     const doc = await addDoc(collection(db, 'items'), { title: item, isDone: false });
     setItem('');
   };
+
+  const renderItem = ({ item }: any) => {
+    return (
+      <Text>{item.title}</Text>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -18,6 +49,11 @@ const List = () => {
         <TextInput style={styles.input} placeholder='Add item' onChangeText={(text: string) => setItem(text)} value={item} />
         <Button onPress={addItem} title='Add' disabled={item === ''} />
       </View>
+      { items.length > 0 && (
+        <View>
+          <FlatList data={items} renderItem={(item) => renderItem(item)} keyExtractor={(item: Item) => item.id} />
+        </View>
+      )}
     </View>
   )
 }
@@ -31,7 +67,8 @@ const styles = StyleSheet.create({
   },
   form: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20
   },
   input: {
     flex: 1,
