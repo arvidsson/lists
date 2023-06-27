1c6aeaffe521154ac5a860f38cccc5212c516232
arvidsson <arvidsson@gmail.com>
2023-06-27 08:39:10 +0200

    fixed login input fields

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,77 +1,85 @@
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
+  // how to signup; I will currently handle that manually in firebase
   const signup = async () => {
     setLoading(true);
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   const login = async () => {
-
     setLoading(true);
     try {
       const user = await signInWithEmailAndPassword(auth, email, password);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        textContentType='emailAddress'
-        autoCapitalize='none'
-        onChangeText={setEmail}
-        value={email}
-      />
-      <TextInput 
-        style={styles.input}
-        placeholder="Password"
-        textContentType='password'
-        autoCapitalize='none'
-        secureTextEntry={true}
-        onChangeText={(text: string) => setPassword(text)}
-        value={password}
-      />
-      <Button onPress={signup} title ="Signup" />
-      <Button onPress={login} title ="Login" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          textContentType="emailAddress"
+          autoCapitalize="none"
+          onChangeText={(text: string) => setEmail(text)}
+          value={email}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          textContentType="password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text: string) => setPassword(text)}
+          value={password}
+        />
+      </View>
+      <Button onPress={login} title="Login" />
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    margin: 20
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 50,
+  },
+  inputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 25,
   },
   input: {
+    flex: 1,
     height: 40,
     borderWidth: 1,
     borderRadius: 4,
-    padding: 20,
+    padding: 10,
     backgroundColor: '#fff',
-    marginBottom: 10
   },
 });
